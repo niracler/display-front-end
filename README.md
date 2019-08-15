a10fa07e52fc2d954ff470f411533fc59da98330
Niracler <1026037967@qq.com>
2019-08-15 22:01:03 +0800

    开始熟悉

--- README.md
+++ README.md
@@ -1,29 +1,36 @@
 # gamenews
 
+游戏资讯平台前端部分
+
 ## Project setup
 ```shell script
 npm install
 ```
 
 ### Compiles and hot-reloads for development
-```
+```shell script
 npm run serve
 ```
 
 ### Compiles and minifies for production
-```
+```shell script
 npm run build
 ```
 
 ### Run your tests
-```
+```shell script
 npm run test
 ```
 
 ### Lints and fixes files
-```
+```shell script
 npm run lint
 ```
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## 参考文章
+
+- [vue-cli3出现Invalid Host header的解决方案](https://blog.csdn.net/guzhao593/article/details/85918869)
+- [Vue.js 教程](https://www.runoob.com/vue2/vue-tutorial.html)
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
+<!--让我好好地享受一下你们吧-->
+<!--下面这个是展示模板-->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+<!--        <img alt="Vue logo" src="./assets/logo.png">-->
+        <!--  原来组件在这里是以标签的这种方法来使用的  -->
+<!--        <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+        <h1>{{ msg }}</h1>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue' //导入组件，组件的运行机制究竟是怎么样的？
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+          // eslint-disable-next-line vue/no-unused-components
+            HelloWorld
+        },
+        data() {
+            return {
+              msg: '我们这里是一个游戏新闻资讯平台！！'
+            }
+        }
+    }
 </script>
 
+<!--显而易见这是样式代码-->
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
