d599bfa87c444c3f5c7250d9ea8cb38b35b179fd
Niracler <1026037967@qq.com>
2019-08-16 10:36:08 +0800

    加了一下侧边的文章类型

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,36 @@
         <!--        <img alt="Vue logo" src="./assets/logo.png">-->
         <!--  原来组件在这里是以标签的这种方法来使用的  -->
         <!--        <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+        <br>
         <h1>{{ msg }}</h1>
 
-        <div v-for="(article) in data.results">
-            <br>
-            <h3 class="text-primary">
-                <a>{{ article.title }}</a>
-            </h3>
-            <p>
-                {{ article.content }}
-            </p>
-            <small>Read More</small>
+        <div class="row">
+            <div class="col-8">
+                <div v-for="article in articles">
+                    <br>
+                    <h3 class="text-primary">
+                        <a>{{ article.title }}</a>
+                    </h3>
+                    <p>
+                        {{ article.content }}
+                    </p>
+                    <small>Read More</small>
+                </div>
+            </div>
+
+            <div class="card h-25">
+                <div class="card-header">Category</div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item list-group-item-action d-flex justify-content-between align-content-center"
+                        v-for="category in categories">
+                        <a href="#">
+                            {{ category.name }}
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
+
 
         <ul class="pagination">
             <li class="page-item disabled">
@@ -50,13 +68,20 @@
         data() {
             return {
                 msg: '我们这里是一个游戏新闻资讯平台！！',
-                data: null
+                articles: null,
+                categories: null
             }
         },
         mounted() {
             axios
                 .get('http://plrom.niracler.com:8000/api/article/')
-                .then(response => (this.data = response.data))
+                .then(response => (this.articles = response.data.results))
+                .catch(function (error) { // 请求失败处理
+                    console.log(error);
+                });
+            axios
+                .get('http://plrom.niracler.com:8000/api/category/')
+                .then(response => (this.categories = response.data.results))
                 .catch(function (error) { // 请求失败处理
                     console.log(error);
                 });
