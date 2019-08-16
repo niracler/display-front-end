6cc8f726ff9a6b4e5bb20cb179643d8655262818
Niracler <1026037967@qq.com>
2019-08-16 16:53:09 +0800

    这个翻页功能太假了

--- src/App.vue
+++ src/App.vue
@@ -2,103 +2,120 @@
 <!--下面这个是展示模板-->
 <template>
     <div class="container" id="app">
-        <!--        <img alt="Vue logo" src="./assets/logo.png">-->
-        <!--  原来组件在这里是以标签的这种方法来使用的  -->
-        <!--        <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-        <br>
-        <h1>{{ msg }}</h1>
 
         <div class="row">
-            <div class="col-8">
-                <div v-for="article in articles">
-                    <br>
-                    <h3 class="text-primary">
-                        <a>{{ article.title }}</a>
-                    </h3>
-                    <p>
-                        {{ article.content | msgFormat }}
-                    </p>
-                    <small>Read More</small>
-                </div>
-            </div>
+            <div class="col-md-8">
 
-            <div class="card h-25">
-                <div class="card-header">Category</div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item list-group-item-action d-flex justify-content-between align-content-center"
-                        v-for="category in categories">
-                        <a href="#">
-                            {{ category.name }}
-                        </a>
+                <h1 class="my-4">
+                    {{ msg }}
+                    <small></small>
+                </h1>
+
+                <!-- Blog Post -->
+                <div class="card mb-4" v-for="article in articles">
+                    <!--                    <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">-->
+                    <div class="card-body">
+                        <h2 class="card-title">{{ article.title }}</h2>
+                        <p class="card-text">
+                            {{ article.content | msgFormat }}
+                        </p>
+                        <a href="#" class="btn btn-primary">Read More &rarr;</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        Posted on {{ article.publish_time | timeFormat }} by
+                        <a href="#">{{ article.website_name }}</a>
+                    </div>
+                </div>
+
+                <!-- Pagination -->
+                <ul class="pagination justify-content-center mb-4">
+                    <li class="page-item">
+                        <a class="page-link" href="#" @click="previousPage()">&larr; Older</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#" @click="nextPage()">Newer &rarr;</a>
                     </li>
                 </ul>
             </div>
+
+            <SideBar></SideBar>
         </div>
-
-
-        <ul class="pagination">
-            <li class="page-item disabled">
-                <span class="page-link">Previous</span>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item active" aria-current="page">
-      <span class="page-link">
-        2
-        <span class="sr-only">(current)</span>
-      </span>
-            </li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item">
-                <a class="page-link" href="#">Next</a>
-            </li>
-        </ul>
     </div>
-
 </template>
 
 <script>
     import HelloWorld from './components/HelloWorld.vue' //导入组件，组件的运行机制究竟是怎么样的？
-
-
+    import SideBar from "./components/SideBar";
+    import axios from 'axios'
 
     export default {
         name: 'app',
         components: {
             // eslint-disable-next-line vue/no-unused-components
-            HelloWorld
+            HelloWorld,
+            SideBar
         },
         data() {
             return {
                 msg: '我们这里是一个游戏新闻资讯平台！！',
+                baseUrl: 'http://plrom.niracler.com:8000/api/article/?p=',
+                currPage: 1,
                 articles: null,
-                categories: null
+                next: null,
+                previous: null
             }
         },
-        mounted() {
-            axios
-                .get('http://plrom.niracler.com:8000/api/article/')
-                .then(response => (this.articles = response.data.results))
-                .catch(function (error) { // 请求失败处理
-                    console.log(error);
-                });
-            axios
-                .get('http://plrom.niracler.com:8000/api/category/')
-                .then(response => (this.categories = response.data.results))
-                .catch(function (error) { // 请求失败处理
-                    console.log(error);
-                });
+        methods: {
+            getAllList(currPage = this.currPage) {
+                var url = this.baseUrl + currPage;
+                axios
+                    .get(url)
+                    .then(response => {
+                        if (response.status === 200) {
+                            this.articles = response.data.results;
+                            this.previous = response.data.previous;
+                            this.next = response.data.next;
+                        } else {
+                            alert('获取数据失败！！！')
+                        }
+                    })
+                    .catch(function (error) { // 请求失败处理
+                        self.console.log(error);
+                    });
+            },
+            nextPage(){
+                this.currPage += 1;
+                this.getAllList();
+            },
+            previousPage(){
+                this.currPage -= 1;
+                this.getAllList();
+            }
         },
+        created() {
+            this.getAllList();
+        },
+        mounted() {
+
+        },
+        //这里放的是局部的过滤器
+        filters: {
+            timeFormat: function (dateStr) {
+                var dt = new Date(dateStr);
+
+                var y = dt.getFullYear();
+                var m = dt.getMonth();
+                var d = dt.getDay();
+                var hh = dt.getHours();
+                var mm = dt.getMinutes();
+                var ss = dt.getSeconds();
+
+                return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+            }
+        }
     }
 </script>
 
 <!--显而易见这是样式代码-->
 <style>
-    /*#app {*/
-    /*    font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
-    /*    -webkit-font-smoothing: antialiased;*/
-    /*    -moz-osx-font-smoothing: grayscale;*/
-    /*    text-align: center;*/
-    /*    color: #2c3e50;*/
-    /*    margin-top: 60px;*/
-    /*}*/
 </style>
