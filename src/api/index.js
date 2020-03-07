fd0c0c8ee87cef166862cbb5414f1122e412aafa
Niracler <1026037967@qq.com>
2020-03-07 14:52:26 +0800

    add gin api

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+export const ginHost = process.env.VUE_APP_GIN_HOST;
 export const flaskHost = process.env.VUE_APP_FLASK_HOST;
 export const proHost = process.env.VUE_APP_PRO_HOST;
 export const mediaHost = process.env.VUE_APP_MEDIA_HOST;
@@ -7,7 +8,7 @@ export const host = proHost;
 
 //获取游戏实体信息
 export const entityInfo = params => {
-    return axios.get(`${flaskHost}/api/entity/`, params) //原来这里就用到了自定义插值，模板字符串以及箭头定义函数
+    return axios.get(`${ginHost}/api/v1/game`, params) //原来这里就用到了自定义插值，模板字符串以及箭头定义函数
 };
 
 //获取redis信息
